c86a320a81f9d88a5bfe7b0c64cecde7bfe97a69
Daniyal9988 <sdani9988@gmail.com>
2024-08-06 05:47:50 +0300

    more notes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
+use \App\Models\Task;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,58 +15,58 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
+// class Task
+// {
+//     public function __construct(
+//         public int $id,
+//         public string $title,
+//         public string $description,
+//         public ?string $long_description,
+//         public bool $completed,
+//         public string $created_at,
+//         public string $updated_at
+//     ) {
+//     }
+// }
 
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
+// $tasks = [
+//     new Task(
+//         1,
+//         'Buy groceries',
+//         'Task 1 description',
+//         'Task 1 long description',
+//         false,
+//         '2023-03-01 12:00:00',
+//         '2023-03-01 12:00:00'
+//     ),
+//     new Task(
+//         2,
+//         'Sell old stuff',
+//         'Task 2 description',
+//         null,
+//         false,
+//         '2023-03-02 12:00:00',
+//         '2023-03-02 12:00:00'
+//     ),
+//     new Task(
+//         3,
+//         'Learn programming',
+//         'Task 3 description',
+//         'Task 3 long description',
+//         true,
+//         '2023-03-03 12:00:00',
+//         '2023-03-03 12:00:00'
+//     ),
+//     new Task(
+//         4,
+//         'Take dogs for a walk',
+//         'Task 4 description',
+//         null,
+//         false,
+//         '2023-03-04 12:00:00',
+//         '2023-03-04 12:00:00'
+//     ),
+// ];
 
 
 //named routes 
@@ -86,27 +87,70 @@ $tasks = [
 //     return 'you got somewhere my g';
 // });
 
-Route::get('/', function () use($tasks) {
+Route::get('/', function ()  {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/taks', function () use($tasks) {
+// Route::get('/taks', function () use($tasks) {
+//     return view('index'
+//     ,
+//     [
+//         'tasks' => $tasks
+//     ]
+// );
+// })->name('tasks.index');
+
+
+// Route::get('/taks/{id}',function ($id) use ($tasks){
+//     $task = collect($tasks)->firstWhere('id',$id);
+
+//     if(!$task)
+//     {
+//         return ('task not there');
+//     }
+//     return view('show', [ 'task' => $task]);
+
+// })->name('tasks.show');
+
+
+Route::get('/taks', function ()  {
     return view('index'
     ,
     [
-        'tasks' => $tasks
+        'tasks' => 
+        // \App\Models\Task::all()
+        // \App\Models\Task::latest()->where('completed' ,  true)->get()
+        Task::latest()->get()
     ]
 );
 })->name('tasks.index');
 
+Route::view('/tasks/create' , 'create')
+->name('tasks.create');
 
-Route::get('/taks/{id}',function ($id) use ($tasks){
-    $task = collect($tasks)->firstWhere('id',$id);
 
-    if(!$task)
-    {
-        return ('task not there');
-    }
-    return view('show', [ 'task' => $task]);
+Route::get('/taks/{id}',function ($id) {
+    
+    return view('show', 
+    [ 'task' => Task::findOrFail($id)]
+);
 
 })->name('tasks.show');
+
+
+Route::post('/tasks' , function(Request $request){
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show' , ['id' => $task->id]);
+})->name('tasks.store');
